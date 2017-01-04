c8a619b0fc8f0e4ec6338c09a2c72c769f9f5d13
thi49 <thilak.chelliah49@gmail.com>
2017-01-04 14:09:27 +0000

    Added mongo db,js modules

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose =  require('mongoose');
+mongoose.connect("mongodb://" + process.env.IP + ":27017/data/TechRegistrydb",function(err, db){
+  if (err) {
+    return console.dir(err);
+  }
+});
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -12,18 +18,24 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/angular'));
 //app.use('/api', api); // redirect API calls
 
 // catch 404 and forward to error handler
@@ -44,6 +56,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 app.listen(15454);
+
 
 console.log('Server running at ' + process.env.IP + ':' + process.env.PORT);
 module.exports = app;
