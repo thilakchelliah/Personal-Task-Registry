0686c35356b4b935a1abb53f6a4359147769520d
thilakchelliah <thilak.chelliah49@gmail.com>
2017-01-08 21:18:07 +0530

    added jquery and bootstrap to package.json

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap3/dist/js'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap3/dist/css'));
 app.use('/fontcss', express.static(__dirname + '/node_modules/font-awesome'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/js', express.static(__dirname + '/node_modules/angular'));
